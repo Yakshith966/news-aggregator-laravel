5b0a241b3e5a0a2adb9decbbc80a0cdefd364a8c
yakshithkumar948 <yakshithkumar948@gmail.com>
2025-01-30 20:54:45 +0530

    installed swagger api

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\NewsFeedController;
+use App\Http\Controllers\PreferenceController;
+use App\Http\Controllers\ResetPasswordController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +21,21 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+// Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::post('forgot-password', [ResetPasswordController::class, 'forgotPassword'])->name('password.request');
+Route::post('reset-password', [ResetPasswordController::class, 'resetPassword'])->name('password.reset');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
+
+    Route::get('articles', [ArticleController::class, 'index'])->name('article.index');
+    Route::get('articles/{article}', [ArticleController::class, 'show'])->name('article.show');
+
+    Route::post('preferences', [PreferenceController::class, 'store']);
+    Route::get('preferences', [PreferenceController::class, 'index']);
+
+    Route::get('newsfeed',  [NewsFeedController::class, 'index']);
 });
