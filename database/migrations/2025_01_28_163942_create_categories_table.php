a349d63de104617283c2925e4afbae69a1aab7ad
yakshithkumar948 <yakshithkumar948@gmail.com>
2025-01-30 20:13:15 +0530

    added migration for the categories,authors,sources and the preferences table

---
+++ database/migrations/2025_01_28_163942_create_categories_table.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('authors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('sources', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->foreignId('category_id')->constrained();
+            $table->foreignId('author_id')->constrained();
+            $table->foreignId('source_id')->constrained();
+            $table->timestamp('published_at')->nullable();
+            $table->timestamps();
+
+            $table->index('title');
+            $table->index('published_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('authors');
+        Schema::dropIfExists('sources');
+        Schema::dropIfExists('articles');
+    }
+}
