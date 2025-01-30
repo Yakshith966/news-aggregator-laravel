53a561e47a7a7a04d20217fb911de0057d920f78
yakshithkumar948 <yakshithkumar948@gmail.com>
2025-01-30 20:14:48 +0530

    created model for the tables and also assigned relationship between the tables

---
+++ app/Models/Article.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Article extends Model
+{
+    /** @use HasFactory<\Database\Factories\ArticleFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'category_id',
+        'author_id',
+        'source_id',
+        'published_at',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function source(): BelongsTo
+    {
+        return $this->belongsTo(Source::class);
+    }
+}
